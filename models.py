21bccfc095c86c529b0d7e1b8ddd104b448522f1
Anson <anson32330200@gmail.com>
2016-12-01 19:29:15 -0800

    Add hashing and validate functions

--- models.py
+++ models.py
@@ -1,4 +1,8 @@
+import logging
 from google.appengine.ext import db
+
+from utils.hashing_salt_functions import *
+from utils.validate_info import *
 
 
 class Post(db.Model):
@@ -8,3 +12,43 @@ class Post(db.Model):
     subject = db.StringProperty(required=True)
     content = db.TextProperty(required=True)
     created = db.StringProperty(required=True)
+
+
+class User(db.Model):
+    """
+    Model for user
+    """
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+
+    @classmethod
+    def get_by_name(cls, name):
+        if not valid_username(name): return
+        q = db.GqlQuery("SELECT * FROM User WHERE name = '%s'" % name)
+        return q.get()
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        user = User(name=name, pw_hash=pw_hash, email=email)
+        user.put()
+        return user
+
+    @classmethod
+    def login(cls, name, pw):
+        if valid_username(name):
+            user = cls.get_by_name(name)
+            if user and valid_pw(name, pw, user.pw_hash):
+                return user
+
+    @classmethod
+    def print_to_log(cls):
+        logging.info('printing database')
+        q = db.GqlQuery('SELECT * FROM User')
+        for u in q:
+            logging.info(u.name + ' ' + u.pw_hash)
+
+    @classmethod
+    def delete_all(cls):
+        db.delete(User.all())
---
+++ utils/hashing_salt_functions.py
@@ -0,0 +1,62 @@
+import hashlib
+import hmac
+import random
+import string
+
+from secret import SECRET
+
+
+def hash_str(s):
+    """Hashes given string s and returns result"""
+    return hmac.new(SECRET, s).hexdigest()
+
+
+def make_secure_val(s):
+    """returns a string of the format s|hash(s)"""
+    return s + '|' + hash_str(s)
+
+
+def check_secure_val(h):
+    """
+    :param h: String of the format s|HASH
+    :return: s if hash_str(s) == HASH, otherwise None
+    """
+    if not h: return None
+    strs = h.split('|')
+    if not strs or len(strs) != 2: return None
+    s = strs[0]
+    if make_secure_val(s) == h: return s
+
+
+def make_salt():
+    chars = string.letters  # + string.digits
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(5))
+
+
+def make_pw_hash(name, pw, salt=make_salt()):
+    """
+    :param name: username
+    :param pw: user password
+    :param salt: use salt if provided, otherwise generate random salt
+    :return: string of the format HASH(name + pw + salt),salt
+    """
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+
+def valid_pw(name, pw, h):
+    """
+    :param name: username
+    :param pw: user password
+    :param h: string of the format HASH(name + pw + salt),salt
+    :return: True if a user's password matches its hash
+    """
+    if not name or not pw or not h: return False
+    salt = h.split(',')[1]
+    return make_pw_hash(name, pw, salt) == h
+
+
+if __name__ == '__main__':
+    h = make_pw_hash('spez', 'hunter2')
+    print h
+    print valid_pw('spez', 'hunter2', h)
